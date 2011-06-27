4e22188929d7418e8459dfe747edcf0e89360428
Jered Odegard <jered@soromi.com>
2011-06-27 12:18:51 -0700

    Restructured Class
    added CatFactory with Prototype Design Pattern

--- index.php
+++ index.php
@@ -1,7 +1,32 @@
 <?php
-	class CatFactory {}
+
+	class Hair {}
+	class ShortHair extends Hair {}
+	class LongHair extends Hair {}
 	
-	interface Hair {}
-	class BlackHair implements Hair {}
-	class WhiteHair implements Hair {}
+	class Eyes {}
+	class BrownEyes extends Eyes {}
+	class GreenEyes extends Eyes {}
+	class BlueEyes extends Eyes {}
+	
+	class CatFactory {
+		private $Hair;
+		private $Eyes;
+		
+		function __construct(Hair $hair, Eyes $eyes) {
+			$this->Hair = $hair;
+			$this->Eyes = $eyes;
+		}
+		
+		function getHair() {
+			return clone $this->Hair;
+		}
+		function getEyes() {
+			return clone $this->Eyes;
+		}
+	}
+	
+	$Factory = new CatFactory(new ShortHair(), new GreenEyes());
+	print_r($factory->getHair());
+	print_r($factory->getEyes());
 ?>
