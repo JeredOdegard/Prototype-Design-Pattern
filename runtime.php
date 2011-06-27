bea240ca8cda63c366edd1ce6d153b44c1a70124
Jered Odegard <jered@soromi.com>
2011-06-27 15:21:13 -0700

    Update to Runtime Environment
    added use of config file

--- runtime.php
+++ runtime.php
@@ -1,9 +1,14 @@
 <?php
-	require_once 'cat.php';
+	require_once dirname(__FILE__) . '/cat.php';
+	require_once dirname(__FILE__) . '/config/config.php';
 	
 	// Runtime Environment
 	try {
-		$Factory = new CatFactory(new ShortHair(), new GreenEyes());
+		$Config = Config::getInstance();
+		$Config->CatHair = new LongHair();
+		$Config->CatEyes = new BlueEyes();
+		
+		$Factory = new CatFactory($Config->CatHair, $Config->CatEyes);
 		print($Factory->getHair());
 		print($Factory->getEyes());
 	}
