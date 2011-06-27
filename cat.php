010206229e855e8a43c23fd0d68638e8a9059568
Jered Odegard <jered@soromi.com>
2011-06-27 14:45:43 -0700

    Seperated run-time environment from structure

---
+++ cat.php
@@ -0,0 +1,50 @@
+<?php
+	interface Attribute {
+		function __toString();
+	}
+	abstract class Hair implements Attribute {}
+	class ShortHair extends Hair {
+		public function __toString() {
+			return "I have short hair.\n";
+		}
+	}
+	class LongHair extends Hair {
+		public function __toString() {
+			return "I have long hair.\n";
+		}
+	}
+
+	abstract class Eyes implements Attribute {}
+	class BrownEyes extends Eyes {
+		public function __toString() {
+			return "I have brown eyes.\n";
+		}
+	}
+	class GreenEyes extends Eyes {
+		public function __toString() {
+			return "I have green eyes.\n";
+		}
+	}
+	class BlueEyes extends Eyes {
+		public function __toString() {
+			return "I have blue eyes.\n";
+		}
+	}
+
+	class CatFactory {
+		private $Hair;
+		private $Eyes;
+	
+		function __construct(Hair $hair, Eyes $eyes) {
+			$this->Hair = $hair;
+			$this->Eyes = $eyes;
+		}
+	
+		function getHair() {
+			return clone $this->Hair;
+		}
+		function getEyes() {
+			return clone $this->Eyes;
+		}
+	}
+?>
